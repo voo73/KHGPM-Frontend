a14ccda3c86a1f093854a4f27257059c30511c82
voo73 <voock9@gmail.com>
2023-03-07 16:29:43 +0900

    init: [김다연] 저장소 초기화

---
+++ kimdayeon/src/components/lecture/componentTest/LocalComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ initialValue }}
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: "LocalComponent",
+    props: {
+        initialValue: Number
+        //타입 지정 
+    }
+    
+  }
+  
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ kimdayeon/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <div id="home">
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/bind-test">BindTest</router-link> |
+        <router-link to="/click-test">클릭 테스트</router-link> |
+        <router-link to="/component-test">컴포넌트 테스트</router-link> |
+        <router-link to="/emit-test-board-game">오목게임</router-link> |
+        <router-link to="/axiaos-test-page">Vue에서 Spring으로 데이터 전송하기</router-link> |
+        <router-link to="/board-list-page">게시판</router-link> |
+      </nav>
+      <router-view/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+
+  components: {
+  },
+}
+
+</script>
